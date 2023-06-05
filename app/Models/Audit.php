17573f150dd07ec4616d86c4384a8d51ee0973c2
Thiago <throcha3@gmail.com>
2023-06-04 21:39:53 -0300

    fix fillable fields in audit model

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -7,14 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Audit extends Model
 {
-    const EVENT_UPDATE = 'update';
+    const EVENT_TYPE_UPDATED = '1';
 
     protected $fillable = [
         'user_id',
-        'event',
+        'event_type',
         'model_id',
         'old_data',
         'new_data',
-        'diff_data'
+        'diff_data',
+        'table_name',
     ];
 }
