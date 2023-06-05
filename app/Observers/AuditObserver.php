80e5dcf4ee8f5cc75351bfa5d489359f44ca11b9
Thiago <throcha3@gmail.com>
2023-06-04 21:40:09 -0300

    use constants instead of brute value in observers

--- app/Observers/AuditObserver.php
+++ app/Observers/AuditObserver.php
@@ -2,12 +2,13 @@
 
 namespace App\Observers;
 
+use App\Models\Audit;
 use App\Services\AuditService;
 
 class AuditObserver
 {
     public function updated($model)
     {
-        AuditService::createSingleEventAudit($model, 'updated');
+        AuditService::createSingleEventAudit($model, Audit::EVENT_TYPE_UPDATED);
     }
 }
